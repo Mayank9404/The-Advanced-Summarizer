7fa1029db0ee5b2675845827c97fcc9de83cc011
Mayank Kumar <54901110+Mayank9404@users.noreply.github.com>
2022-08-09 21:54:27 +0530

    Update settings.py

--- backend/precis/precis/settings.py
+++ backend/precis/precis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-_z=cw%t&g11gr4jf_$cf1$pt!au_!outx5jdj3ut3=810&bf8y'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
